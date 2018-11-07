30b64c026b78358cc7d12493cd1e562ea81c5597
Andrey Herrera <kahs_kevin@hotmail.com>
2018-11-06 23:25:35 -0500

    Start

---
+++ Model/template/footer.php
@@ -0,0 +1,11 @@
+      <?php echo $google_analytics; ?> 
+      <!-- Main Footer -->
+      <footer class="main-footer">
+        <!-- To the right -->
+        <div class="pull-right hidden-xs">
+          Desarrollado por Instance Shape SAS
+        </div>
+        <!-- Default to the left -->
+        <strong><a href="index.php"><?php echo escape($title); ?></a> <?php auto_copyright('','All Rights Reserved'); ?>.</strong>
+      </footer>
+      
